324d2f8eba801a90ae870576bf6602ce43298cf4
Maran <dgmmaran@gmail.com>
2026-07-14 06:54:36 +0530

    Updated system prompt for HR chatbot

--- llm.py
+++ llm.py
@@ -6,18 +6,19 @@ load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+
 def query_llm_with_context(query: str, context: str):
     system_content = """You are a helpful assistant for answering user queries based on provided context. 
     use the context to provide accurate and relevant answers. Do not make assumptions beyond the context provided.
     If the context does not contain enough information to answer the query, 
-    let the user know that you cannot provide an answer based on the given context.
+    let the user know that you cannot provide an answer based on the given context.Answer in a maximum of 5 bullet points. If the answer is not found in the context, say 'I couldn't find this information in the HR policy
     """
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": system_content},
-            {"role": "user", "content": f"Query: {query}\n\nContext:\n{context}"}
+            {"role": "user", "content": f"Query: {query}\n\nContext:\n{context}"},
         ],
-        temperature=0.4
+        temperature=0.4,
     )
     return response.choices[0].message.content
